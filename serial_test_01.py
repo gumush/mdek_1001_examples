d741586ebf308ebb7fc896f6043cc979fba4fc70
Hikmet Gümüş <gumush@gmail.com>
2019-01-17 11:47:17 +0300

    Add files via upload
    updated blank line counter

--- serial_test_01.py
+++ serial_test_01.py
@@ -10,16 +10,19 @@ def start_serial():
 
 
 def read_msg(msg):
-    response = ""
+    bl = 0
     ser.flushOutput()
+    ser.flushInput()
     msg = msg + "\r"
     msg=msg.encode()
     ser.write(msg)
     time.sleep(0.1)
-    while response != "".encode():
+    while bl < 3:
         response = ser.readline()
-        time.sleep(0.1)
-        if response != "dwm> ".encode() and response.decode().strip() != msg.decode().strip():
+        if response == "".encode():
+            bl = bl+1
+        time.sleep(0.2)
+        if response != "dwm> ".encode() and response.decode().strip() != msg.decode().strip() and response != "".encode():
             # do whatever you want with this response
             print(response.decode().strip())
     ser.flushInput()
